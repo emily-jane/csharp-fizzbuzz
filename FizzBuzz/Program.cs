29c72fe9f3b19b03f45da75025b38c1bced824bd
Emily Carson <emilyjanecarson@gmail.com>
2015-11-03 17:49:36 +0000

    created fizzbuzz in csharp, 5 tests passing

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FizzBuzz
+{
+	class FizzBuzzMain
+	{
+		public List<string> GetFizzBuzz(int ceiling)
+		{
+			var sequence = new List<string>();
+			for (var i = 1; i <= ceiling; i++)
+			{
+				sequence.Add (GetNextFizzBuzz(i));
+			}
+			return sequence;
+		}
+		public string GetNextFizzBuzz(int position)
+		{
+			var nextFizzBuzz = string.Empty;
+			var isFizzOrBuzz = false;
+
+			if (position % 3 == 0) {
+				nextFizzBuzz = "Fizz";
+				isFizzOrBuzz = true;
+			} else if (position % 5 == 0) {
+				nextFizzBuzz = "Buzz";
+				isFizzOrBuzz = true;
+			} else {
+				nextFizzBuzz = position.ToString ();
+			}
+			if (isFizzOrBuzz && position % 3 == 0 && position % 5 == 0) {
+				nextFizzBuzz = "FizzBuzz";
+			}
+			return nextFizzBuzz;
+		}
+	}
+}
---
+++ FizzBuzz/Test.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace FizzBuzz
+{
+	[TestFixture]
+	public class Tests
+	{
+		[Test]
+		public void TestFizzBuzz_ShouldReturnTwoIntegers()
+		{
+			var fizzbuzz = new FizzBuzzMain();
+			var expected = new List<string>{ "1", "2" };
+
+			var actual = fizzbuzz.GetFizzBuzz(2);
+
+			CollectionAssert.AreEqual(expected, actual);
+		}
+		[Test]
+		public void TestFizzBuzz_ShouldReturnFirstThreeItems()
+		{
+			var fizzbuzz = new FizzBuzzMain();
+			var expected = new List<string> { "1", "2", "Fizz" };
+
+			var actual = fizzbuzz.GetFizzBuzz(3);
+
+			CollectionAssert.AreEqual(expected, actual);
+		}
+		[Test]
+		public void TestFizzBuzz_ShouldReturnFirstFiveItems()
+		{
+			var fizzbuzz = new FizzBuzzMain ();
+			var expected = new List<string>{ "1", "2", "Fizz", "4", "Buzz" };
+
+			var actual = fizzbuzz.GetFizzBuzz (5);
+
+			CollectionAssert.AreEqual (expected, actual);
+		}
+		[Test]
+		public void TestFizzBuzz_Given15thPosition_ShouldReturnFizzBuzz()
+		{
+			var fizbuzz = new FizzBuzzMain();
+			var position = 15;
+			var expected = "FizzBuzz";
+
+			var actualFizzBuzzList = fizbuzz.GetFizzBuzz(position);
+			var actual = actualFizzBuzzList.ElementAt(position - 1);
+
+			Assert.AreEqual(expected, actual);
+		}
+	}
+}
+
